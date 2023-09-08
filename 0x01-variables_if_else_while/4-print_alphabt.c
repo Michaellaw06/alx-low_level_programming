6303a6c2ab8ec7892cc8c3efc1cb7cef3bf39ac8
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-09 00:47:21 +0100

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase, excluding 'e' and 'q'.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
