b8b058a8c5c2ed3e74e645a057629faca58d3b59
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-09 01:05:22 +0100

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Program that prints all single-digit numbers of base 10,
- *        followed by a new line.
+ * followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char digit;
+	char digit;
 
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        putchar(digit);
-    }
-    putchar('\n');
+	for (digit = '0'; digit <= '9'; digit++)
+	{
+		putchar(digit);
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
