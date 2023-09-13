d8a0e787aa6909e998c03780933c1de0132a107d
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 03:05:57 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,10 +10,10 @@ int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return 1; /* c is an alphabetic character */
+		return (1); /* c is an alphabetic character */
 	}
 	else
 	{
-		return 0; /* c is not an alphabetic character */
+		return (0); /* c is not an alphabetic character */
 	}
 }
