726e2adcdb9f8977c6ec82f653ec9efa7b1d4ffb
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 23:43:19 +0100

    Update 5-main.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,44 +1,32 @@
-#include <stdio.h>
+#include "main.h"
 
-/**
- * print_sign - Prints the sign of a number.
- * @n: The number to check.
- *
- * Return: 1 and prints '+' if n is greater than zero,
- *         0 and prints '0' if n is zero,
- *        -1 and prints '-' if n is less than zero.
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-		putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
-	}
-	else
-	{
-		putchar('-');
-		return (-1);
-	}
-}
-
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	int number = 42; /* Replace with the number you want to check */
-	int result = print_sign(number);
+    int r;
 
-	(void)result; /* To avoid the "unused variable" warning */
-	putchar('\n'); /* Print a newline character */
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
 
-	return (0);
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    return (0);
 }
