df1daf9280dea4f2678c430433bd77b4025a9c72
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 23:17:17 +0100

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int print_sign(int n) {
+    if (n > 0) {
+        printf("+\n");
+        return 1;
+    } else if (n == 0) {
+        printf("0\n");
+        return 0;
+    } else {
+        printf("-\n");
+        return -1;
+    }
+}
+
+int main() {
+    int number = 42; // Replace with the number you want to check
+    int result = print_sign(number);
+    return 0;
+}
