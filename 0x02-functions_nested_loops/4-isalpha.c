2ba955c18969e63623d657997435b3f44b84f043
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 02:54:39 +0100

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,26 @@
 #include "main.h"
+
 /**
- * _isalpha - Checks if a character is alphabetic.
- * @c: The character to check.
+ * main - Check if _isalpha works
  *
- * Return: 1 if c is alphabetic, 0 otherwise.
+ * Return: Always 0
  */
-int _isalpha(int c)
+int main(void)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    {
-        return 1; // c is an alphabetic character
-    }
-    else
-    {
-        return 0; // c is not an alphabetic character
-    }
+    int r;
+
+    r = _isalpha('H');
+    _putchar(r + '0');
+
+    r = _isalpha('o');
+    _putchar(r + '0');
+
+    r = _isalpha(108);
+    _putchar(r + '0');
+
+    r = _isalpha(';');
+    _putchar(r + '0');
+
+    _putchar('\n');
+    return (0);
 }
