f559ac22f06ad83053c34f7d4e12a7bb1ee90ba0
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 23:29:51 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 
-int print_sign(int n) {
-    if (n > 0) {
-        printf("+\n");
-        return 1;
-    } else if (n == 0) {
-        printf("0\n");
-        return 0;
-    } else {
-        printf("-\n");
-        return -1;
-    }
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 and prints '+' if n is greater than zero,
+ *         0 and prints '0' if n is zero,
+ *        -1 and prints '-' if n is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
 }
 
-int main() {
-    int number = 42; /* Replace with the number you want to check */
-    int result = print_sign(number);
-    (void)result; // To avoid the "unused variable" warning
-    return 0;
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number = 42; /* Replace with the number you want to check */
+	int result = print_sign(number);
+
+	(void)result; /* To avoid the "unused variable" warning */
+	putchar('\n'); /* Print a newline character */
+
+	return (0);
 }
