f62a3082d716de15cca33271901b79866beff096
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 00:00:30 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,32 +1,19 @@
 #include "main.h"
 
 /**
-
- * _abs - Computers the absolute
-
- * @c: The number computed
-
- * Return: Absolute value
-
- */
-
+ * _abs - Computes the absolute value of a number.
+ * @c: The number to compute the absolute value for.
+ *
+ * Return: Absolute value of c.
+ */
 int _abs(int c)
-
 {
+	if (c < 0)
+	{
+		int abs_val;
+		abs_val = c * -1;
+		return (abs_val);
+	}
 
-        if (c < 0)
-
-        {
-
-                int abs_val;
-
-                abs_val = c * -1;
-
-                return (abs_val);
-
-        }
-
-        return (c);
-
+	return (c);
 }
-
