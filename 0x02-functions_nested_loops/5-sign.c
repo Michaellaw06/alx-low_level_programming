8aa77dee23f460dfd99a7a651d3235fdac1985dd
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 23:51:34 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,48 +1,28 @@
 #include "main.h"
 
 /**
-
- * print_sign - Printys sum
-
- * @n: The nmber
-
- * Return: 1 for all
-
- */
-
+ * print_sign - Prints the sign of a number.
+ * @n: The number to check.
+ *
+ * Return: 1 if n is greater than zero,
+ *         0 if n is zero,
+ *        -1 if n is less than zero.
+ */
 int print_sign(int n)
-
 {
-
-        if (n > 0)
-
-        {
-
-                _putchar(43);
-
-                return (1);
-
-        }
-
-        else if (n < 0)
-
-        {
-
-                _putchar(45);
-
-                return (-1);
-
-        }
-
-        else
-
-        {
-
-                _putchar(48);
-
-                return (0);
-
-        }
-
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        return (-1);
+    }
+    else
+    {
+        _putchar('0');
+        return (0);
+    }
 }
-
