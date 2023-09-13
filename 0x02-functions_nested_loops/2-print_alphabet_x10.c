1f731ee0ac458cc308374028bcfa0059754ea61a
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-13 02:18:46 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@ void print_alphabet_x10(void)
     char letter;
     int i;
 
-    for (i = 0; i < 10; i++) // Loop to print the alphabet ten times
+    for (i = 0; i < 10; i++) /* Loop to print the alphabet ten times */
     {
         letter = 'a';
-        while (letter <= 'z') // Loop to print each letter
+        while (letter <= 'z') /* Loop to print each letter */
         {
             _putchar(letter);
             letter++;
         }
-        _putchar('\n'); // New line after each set of alphabet
+        _putchar('\n'); /* New line after each set of alphabet */
     }
 }
