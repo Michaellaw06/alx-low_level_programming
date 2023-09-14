8dbe2b9c19dca9e79221226fddbb36b69d34b428
michaellaw06 <tronzybabao@gmail.com>
2023-09-14 14:53:07 -0700

    0x04

---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void) {
+  char c;
+  c = 'A';
+  printf("%c: %d\n", c, isupper(c));
+  c = 'a';
+  printf("%c: %d\n", c, isupper(c));
+  return (0);
+}
+
