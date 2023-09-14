ffef8cdd589ed4b593922d0a5f102857b14f7869
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 04:57:33 +0100

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,28 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h> // Include this for printf
 
-/**
- * print_last_digit - Prints and returns the last digit of a number.
- * @n: The input number.
- *
- * Return: The value of the last digit.
- */
-int print_last_digit(int n) {
-  int last_digit = n % 10;
-  return last_digit;
-}
+int main(void)
+{
+    int r;
 
-int main() {
-  int r;
+    r = print_last_digit(98);
+    printf("%d\n", r);
 
-  r = print_last_digit(98);
-  printf("%d\n", r);
+    r = print_last_digit(0);
+    printf("%d\n", r);
 
-  r = print_last_digit(0);
-  printf("%d\n", r);
+    r = print_last_digit(-1024);
+    printf("%d\n", r);
 
-  r = print_last_digit(-1024);
-  printf("%d\n", r);
-
-  return 0;
+    return (0);
 }
