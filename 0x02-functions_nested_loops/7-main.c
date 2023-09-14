341adbcecd52fa3a7e53cd2443e2cf3d94ee7ca2
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 04:47:41 +0100

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * print_last_digit - Prints and returns the last digit of a number.
+ * @n: The input number.
+ *
+ * Return: The value of the last digit.
+ */
 int print_last_digit(int n) {
   int last_digit = n % 10;
   return last_digit;
