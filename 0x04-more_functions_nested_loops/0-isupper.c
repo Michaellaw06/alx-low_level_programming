78f7b2a5fc54b67cf2f10888cbdca26208c7b08a
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 23:01:18 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <ctype.h> // Include the ctype.h header for isupper
+#include <ctype.h>
 
-int _isupper(int c);
+int my_isupper(int c);
 
 int main(void)
 {
-    char c = 'A'; // Example character 'A'
-    printf("%c: %d\n", c, _isupper(c)); // Use _isupper instead of isupper
+    char c = 'A';
+    printf("%c: %d\n", c, my_isupper(c));
     return (0);
 }
 
-int _isupper(int c)
+int my_isupper(int c)
 {
     if (c >= 'A' && c <= 'Z')
         return (1);
