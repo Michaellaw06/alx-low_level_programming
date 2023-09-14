ac5c72d948240dd651da6fa921ef3881271160b3
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 04:38:38 +0100

    Update 7-main.c

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,32 +1,41 @@
 #include "main.h"
 
 /**
-
- * main - check the code
-
- *
-
- * Return: Always 0.
-
- */
-
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+  int r;
 
-        int r;
-
-        print_last_digit(98);
-
-        print_last_digit(0);
-
-        r = print_last_digit(-1024);
-
-        _putchar('0' + r);
-
-        _putchar('\n');
-
-        return (0);
-
+  print_last_digit(98);
+  print_last_digit(0);
+  r = print_last_digit(-1024);
+  _putchar('0' + r);
+  _putchar('\n');
+  return (0);
 }
 
+/**
+ * print_last_digit - entry point
+ *
+ * Description: function that prints the last digit of a number.
+ *
+ * @a: number to compute last digit
+ *
+ * Return: last digit
+ */
+int print_last_digit(int a)
+{
+  int last_digit;
+
+  last_digit = a % 10;
+
+  // Use `abs()` instead of `last_digit * -1` to comply with Betty style guide.
+  last_digit = abs(last_digit);
+
+  _putchar(last_digit + '0');
+
+  return (last_digit);
+}
