f04a0932b54aa3d5172bec12ee9f54a8af5fe6ea
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 06:05:12 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -6,14 +6,16 @@
  * Description: function that adds two integers and
  * returns the result.
  *
- * @n1: first integer
- * @n2: second integer
+ * @n1: integer one
+ * @n2: integer two
  *
  * Return: sum.
  */
 int add(int n1, int n2)
 {
     int sum;
+
     sum = n1 + n2;
+
     return (sum);
 }
