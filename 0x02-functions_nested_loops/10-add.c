e4935c386f30fbe0b35b8c5bbe475afeac36257b
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 06:02:42 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * Description: function that adds two integers and
+ * returns the result.
+ *
+ * @n1: first integer
+ * @n2: second integer
+ *
+ * Return: sum.
+ */
+int add(int n1, int n2)
+{
+    int sum;
+    sum = n1 + n2;
+    return (sum);
+}
