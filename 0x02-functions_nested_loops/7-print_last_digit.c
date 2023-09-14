ce9c3ce52f0131225c8976d39f037838e00dfab4
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-14 05:46:28 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,16 +11,16 @@
  */
 int print_last_digit(int a)
 {
-    int last_digit;
+	int last_digit;
 
-    last_digit = a % 10;
+	last_digit = a % 10;
 
-    if (last_digit < 0)
-    {
-        last_digit = -last_digit;
-    }
+	if (last_digit < 0)
+	{
+		last_digit = -last_digit;
+	}
 
-    _putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 
-    return (last_digit);
+	return (last_digit);
 }
