ad1305debb875905c3d57a5c6766606dbc86dd1e
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-15 07:31:48 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,7 +12,7 @@ int main(void)
 		}
 	}
 
-	printf("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get ");
+	printf("natural numbers below 10 that are multiples of 3 or 5");
 	printf("3, 5, 6, and 9. The sum of these multiples is %d.\n", sum);
 
 	return (0);
