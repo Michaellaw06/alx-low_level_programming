fec38fa4e71454a07a2497145dae86570d7502a2
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 04:04:07 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+/**
+ * puts2 - Prints every other character of a string, starting with the first character.
+ * @str: Pointer to the string.
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (i % 2 == 0)
+            write(1, &str[i], 1);
+        i++;
+    }
+
+    write(1, "\n", 1);
+}
