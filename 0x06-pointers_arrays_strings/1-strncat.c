48c0de53e1e5662e31b5e80deea1d933ab4d91fd
michaellaw06 <tronzybabao@gmail.com>
2023-09-20 01:06:08 -0700

    0x10

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strncat(char *dest, char *src, int n) {
+    int dest_len = 0;
+    while (dest[dest_len] != '\0')
+        dest_len++;
+
+    for (int i = 0; i < n && src[i] != '\0'; i++)
+        dest[dest_len++] = src[i];
+
+    dest[dest_len] = '\0';
+
+    return dest;
+}
+
+int main() {
+    char dest[20] = "Hello, ";
+    char src[] = "world!";
+    int n = 5;  // Concatenate at most 5 characters from src
+
+    printf("Concatenated string: %s\n", _strncat(dest, src, n));
+
+    return 0;
+}
+
