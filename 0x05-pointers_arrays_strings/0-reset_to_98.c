d8650bf270ba7b47daf6713cd456440bc274c41c
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 03:31:57 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * update_to_98 - Updates the value pointed to by a pointer to 98.
  * @n: Pointer to an integer to be updated.
  */
 void update_to_98(int *n)
 {
-if (n != NULL)
 *n = 98;
 }
