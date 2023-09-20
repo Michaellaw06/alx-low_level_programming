f94ac9491b0915f5d41db922dbd993aca8be7736
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 20:22:37 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-
 char *_strncat(char *dest, char *src, int n) {
-    int dest_len = 0;
-    while (dest[dest_len] != '\0')
-        dest_len++;
+int dest_len = 0;
+while (dest[dest_len] != '\0')
+dest_len++;
 
-    for (int i = 0; i < n && src[i] != '\0'; i++)
-        dest[dest_len++] = src[i];
+for (int i = 0; i < n && src[i] != '\0'; i++)
+dest[dest_len++] = src[i];
 
-    dest[dest_len] = '\0';
+dest[dest_len] = '\0';
 
-    return dest;
+(return dest);
 }
 
-int main() {
-    char dest[20] = "Hello, ";
-    char src[] = "world!";
-    int n = 5;
+main(void) 
+{
+char dest[20] = "Hello, ";
+char src[] = "world!";
+int n = 5;
 
-    printf("Concatenated string: %s\n", _strncat(dest, src, n));
-
-    return 0;
+printf("Concatenated string: %s\n", _strncat(dest, src, n));
+return 0;
 }
 
