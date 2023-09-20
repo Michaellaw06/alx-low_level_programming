2c26426041ee41f7bd23480ba9a495ca07017bbb
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 03:59:14 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,11 @@
  */
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
-
-    while (s[length] != '\0')
-        length++;
-    for (i = length - 1; i >= 0; i--)
-        write(1, &s[i], 1);
-
-    write(1, "\n", 1);
+int length = 0;
+int i;
+while (s[length] != '\0')
+length++;
+for (i = length - 1; i >= 0; i--)
+write(1, &s[i], 1);
+write(1, "\n", 1);
 }
