8d7b6debd15c562a9fe1c04c3344e52a2902f1cb
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 20:34:21 +0100

    Update 0-main.c

--- 0x06-pointers_arrays_strings/0-main.c
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -1,1 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+char *_strcat(char *dest, char *src)
+{
+    int dest_len = 0;
+    while (dest[dest_len] != '\0')
+        dest_len++;
+
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        dest[dest_len] = src[i];
+        dest_len++;
+        i++;
+    }
+
+    dest[dest_len] = '\0';
+    return dest;
+}
+
+int main(void)
+{
+    char dest[30] = "Hello, ";
+    char src[] = "world!";
+    
+    printf("Before concatenation: %s\n", dest);
+    _strcat(dest, src);
+    printf("After concatenation: %s\n", dest);
+    
+    return 0;
+}
 
