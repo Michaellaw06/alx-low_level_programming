fdc847bd2f53013ad06ced56d86b09c69ab48fcf
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 04:26:46 +0100

    Create 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generate_password(char *password, int length) {
+const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+int charset_length = sizeof(charset) - 1;
+
+srand(time(NULL));
+
+for (int i = 0; i < length; i++) {
+password[i] = charset[rand() % charset_length];
+}
+
+password[length] = '\0';
+}
+
+int main() {
+int password_length = 10; // Change this to set the desired password length
+char password[password_length + 1];
+
+generate_password(password, password_length);
+
+printf("Generated Password: %s\n", password);
+
+return 0;
+}
