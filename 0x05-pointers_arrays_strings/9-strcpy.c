2ae7e4d884348eefcd768de3ade4eafda3ae7d6a
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 04:37:59 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,19 +8,22 @@ int charset_length = sizeof(charset) - 1;
 
 srand(time(NULL));
 
-for (int i = 0; i < length; i++) {
+int i;  // Declare 'i' outside the loop as ISO C90 does not allow variable declarations in the loop.
+for (i = 0; i < length; i++) {
 password[i] = charset[rand() % charset_length];
-}
+    }
+
 password[length] = '\0';
 }
 
-int main() {
-int password_length = 10; // Change this to set the desired password length
-char password[password_length + 1];
+int main(void) {
+int password_length = 10;
+char *password = (char *)malloc((password_length + 1) * sizeof(char));
 
 generate_password(password, password_length);
 
 printf("Generated Password: %s\n", password);
 
+free(password); 
 return 0;
 }
