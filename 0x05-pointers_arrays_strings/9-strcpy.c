35b7f90d1a6ae0f674d4e559fcfa0d85ab30cf61
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-20 04:28:22 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * strcpy_custom - Copies the string pointed to by src to the buffer pointed to by dest.
- * @dest: Pointer to the destination buffer.
- * @src: Pointer to the source string.
- * Return: Pointer to dest.
- */
-char *strcpy_custom(char *dest, const char *src)
-{
-char *original_dest = dest;
+void generate_password(char *password, int length) {
+const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+int charset_length = sizeof(charset) - 1;
 
-while (*src != '\0')
-{
-*dest = *src;
-dest++;
-src++;
+srand(time(NULL));
+
+for (int i = 0; i < length; i++) {
+password[i] = charset[rand() % charset_length];
+}
+password[length] = '\0';
 }
 
-*dest = '\0';
+int main() {
+int password_length = 10; // Change this to set the desired password length
+char password[password_length + 1];
 
-return original_dest;
+generate_password(password, password_length);
+
+printf("Generated Password: %s\n", password);
+
+return 0;
 }
