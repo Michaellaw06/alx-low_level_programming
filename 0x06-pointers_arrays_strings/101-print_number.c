72f45e740101867e12655931f5babe8a5d242c4c
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-22 02:18:16 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_number - Function to print an integer.
- * @n: The integer to be printed.
- */
+* print_number - Function to print an integer.
+* @n: The integer to be printed.
+*/
 void print_number(int n)
 {
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10 != 0)
-        print_number(n / 10);
-
-    _putchar((n % 10) + '0');
+if (n < 0) {
+_putchar('-');
+n = -n;
+}
+if (n / 10 != 0)
+print_number(n / 10);
+_putchar((n % 10) + '0');
 }
