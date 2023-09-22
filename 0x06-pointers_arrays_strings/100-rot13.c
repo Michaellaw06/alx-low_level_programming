2a7890407a0e8dab1306511788825ac6697869e5
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-22 02:04:13 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,24 @@
 #include "main.h"
-
 /**
  * print_number - Function to print an integer.
  * @n: The integer to be printed.
  */
 void print_number(int n)
 {
-    unsigned int n1;
-    n1 = (n < 0) ? -n : n;
-
-    if (n < 0)
-        _putchar('-');
-
-    if (n1 / 10 != 0)
-        print_number(n1 / 10);
-
-    _putchar((n1 % 10) + '0');
+unsigned int n1;
+n1 = (n < 0) ? -n : n;
+if (n < 0)
+_putchar('-');
+if (n1 / 10 != 0)
+print_number(n1 / 10);
+_putchar((n1 % 10) + '0');
 }
-
 /**
- * rot13 - Function to apply ROT13 encryption.
- * @str: The string to be encrypted.
- * Return: The encrypted string.
- */
+* rot13 - Function to apply ROT13 encryption.
+* @str: The string to be encrypted.
+* Return: The encrypted string.
+*/
 char *rot13(char *str)
 {
-    return (str);
+return (str);
 }
