2e0839266854dfb88e4d441eb722d467d6e2ccbc
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-26 23:37:50 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,19 +9,21 @@ int main(void)
     sum = 0;
     srand(time(NULL));
 
-    for (i = 0; i < 100; i++)
+    for (i = 0; i < 11; i++)
     {
-        pass[i] = rand() % 78;
-        sum += (pass[i] + '0');
-        putchar(pass[i] + '0');
-
-        if ((2772 - sum) - '0' < 78)
+        if (i == 10)
         {
-            n = 2772 - sum;
-            sum += n;
-            putchar(n + '0');
-            break;
-}
-}
-return 0;
+            pass[i] = (2772 - sum) - '0';
+            sum += pass[i] + '0';
+            putchar(pass[i] + '0');
+        }
+        else
+        {
+            pass[i] = rand() % 78;
+            sum += (pass[i] + '0');
+            putchar(pass[i] + '0');
+        }
+    }
+
+    return 0;
 }
