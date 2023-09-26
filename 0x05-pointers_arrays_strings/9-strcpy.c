9c55ea9871b052a237b4719670f77cd186beb9ca
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-09-26 23:14:58 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,29 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
-void generate_password(char *password, int length) {
-const char charset[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-int charset_length = sizeof(charset) - 1;
-int i;
-srand(time(NULL));
-for (i = 0; i < length; i++) 
+/**
+
+* char *_strcpy - a function that copies the string pointed to by src
+* @dest: copy to
+* @src: copy from
+* Return: string
+*/
+char *_strcpy(char *dest, char *src)
 {
-password[i] = charset[rand() % charset_length];
-}
-
-password[length] = '\0';
-}
-
-int main(void) 
+int l = 0;
+int x = 0;
+while (*(src + l) != '\0')
 {
-int password_length = 10;
-char *password = (char *)malloc((password_length + 1) * sizeof(char));
-
-generate_password(password, password_length);
-
-printf("Generated Password: %s\n", password);
-
-free(password); 
-return 0;
+l++;
+}
+for ( ; x < l ; x++)
+{
+dest[x] = src[x];
+}
+dest[l] = '\0';
+return (dest);
 }
