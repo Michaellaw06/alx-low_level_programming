ca548a2f6317e4ea05f21278f424a0348ff0fd44
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-02 12:41:04 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-*create the objext files
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-*generate the static library
 ar -rc liball.c *.c
-*indexing
 ranlib liball.c
+ar -t libmy.c
+nm libmy.c
