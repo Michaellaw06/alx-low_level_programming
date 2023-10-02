ff9df82d846a22fa3c79f4d5d99ca6bb1fa7f948
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-02 11:58:54 +0100

    Update 2-strlen.c

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,35 +1,16 @@
 #include "main.h"
-
 /**
-
- * _strlen - returns the length of a string
-
- * @s: string
-
- * Return: length
-
- */
-
+ * _strlen - Calculates the length of a string.
+ * @s: Pointer to the string.
+ * Return: Length of the string.
+ */
 int _strlen(char *s)
-
 {
-
-                int longi = 0;
-
- 
-
-                while (*s != '\0')
-
-                {
-
-                            longi++;
-
-                            s++;
-
-                }
-
- 
-
-                return (longi);
-
+int length = 0;
+while (*s != '\0')
+{
+length++;
+s++;
+}
+return (length);
 }
