4cf6987f43f001f0668adb9809ed524f658c4db6
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-04 23:34:14 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -6,11 +6,13 @@ int count_word(char *s)
 int flag, c, w;
 flag = 0;
 w = 0;
-for (c = 0; s[c] != '\0'; c++) 
+for
+(c = 0; s[c] != '\0'; c++) 
 {
 if (s[c] == ' ')
 flag = 0;
-else if (flag == 0) 
+else if
+(flag == 0) 
 {
 flag = 1;
 w++;
@@ -30,7 +32,8 @@ return (NULL);
 matrix = (char **)malloc(sizeof(char *) * (words + 1));
 if (matrix == NULL)
 return (NULL);
-for (i = 0; i <= len; i++) 
+for
+(i = 0; i <= len; i++) 
 {
 if (str[i] == ' ' || str[i] == '\0') 
 {
