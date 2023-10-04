d7dc1516df5504cb6fd97dbd1a737d807bc52353
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-04 23:36:42 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,15 +7,17 @@ int count_word(char *s)
 	flag = 0;
 	w = 0;
 
-	for (c = 0; s[c] != '\0'; c++) {
+	for (c = 0; s[c] != '\0'; c++)
+	{
 		if (s[c] == ' ')
 			flag = 0;
-		else if (flag == 0) {
+		else if (flag == 0)
+		{
 			flag = 1;
 			w++;
 		}
 	}
-	return w;
+	return (w);
 }
 
 char **strtow(char *str)
@@ -29,21 +31,24 @@ char **strtow(char *str)
 	words = count_word(str);
 
 	if (words == 0)
-		return NULL;
+		return (NULL);
 
 	matrix = (char **)malloc(sizeof(char *) * (words + 1));
 
 	if (matrix == NULL)
-		return NULL;
+		return (NULL);
 
-	for (i = 0; i <= len; i++) {
-		if (str[i] == ' ' || str[i] == '\0') {
-			if (c) {
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
 				end = i;
 				tmp = (char *)malloc(sizeof(char) * (c + 1));
 
 				if (tmp == NULL)
-					return NULL;
+					return (NULL);
 
 				while (start < end)
 					*tmp++ = str[start++];
@@ -53,10 +58,11 @@ char **strtow(char *str)
 				k++;
 				c = 0;
 			}
-		} else if (c++ == 0)
+		}
+		else if (c++ == 0)
 			start = i;
 	}
 
 	matrix[k] = NULL;
-	return matrix;
+	return (matrix);
 }
