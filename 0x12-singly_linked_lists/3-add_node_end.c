a3f2fea2d7552cc83f33936c2bd98b5648d54864
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-19 23:34:18 +0100

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,29 +3,32 @@
 #include "lists.h"
 
 /**
- * add_node - Adds a new node at the beginning of a linked list
- * @head: Double pointer to the list_t list
- * @str: New string to add in the node
- *
- * Return: The address of the new element, or NULL if it fails
- */
-list_t *add_node(list_t **head, const char *str)
+* add_node_end - adds a new node at the end of a linked list
+* @head: double pointer to the list_t list
+* @str: string to put in the new node
+* Return: address of the new element, or NULL if it failed
+
+*/
+list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    unsigned int str_len = 0;
-
-    while (str[str_len])
-        str_len++;
-
-    new_node = malloc(sizeof(list_t));
-    if (!new_node)
-        return (NULL);
-
-    new_node->str = strdup(str);
-    new_node->len = str_len;
-    new_node->next = (*head);
-    (*head) = new_node;
-
-    return (*head);
+list_t *new;
+list_t *temp = *head;
+unsigned int len = 0;
+while (str[len])
+len++;
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
+new->str = strdup(str);
+new->len = len;
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
 }
-
+while (temp->next)
+temp = temp->next;
+temp->next = new;
+return (new);
+}
