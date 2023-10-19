4ea4d195b12a5bfdd5e4de9eb7e94be3f45dff7b
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-19 23:22:26 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,21 +2,19 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in the list
- */
+* list_len - returns the number of elements in a linked list
+* @h: pointer to the list_t list
+*
+* Return: number of elements in the list
+*/
 size_t list_len(const list_t *head)
 {
-    size_t count = 0;
-
-    while (head)
-    {
-        count++;
-        head = head->next;
-    }
-
-    return (count);
+size_t count = 0;
+while (head)
+{
+count++;
+head = head->next;
+}
+return (count);
 }
 
