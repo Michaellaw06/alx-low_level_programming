477d1f48dcaa69008133cff61f6f3810a01e0e96
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-25 03:58:14 +0100

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,25 +7,15 @@
  * @head: listint_t list to be freed
 
  */
-
 void free_listint(listint_t *head)
-
 {
-
- listint_t *temp;
-
- while (head)
-
- {
-
- temp = head->next;
-
- free(head);
-
- head = temp;
-
- }
-
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
 
 
