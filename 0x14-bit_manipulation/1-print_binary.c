94a7d998a3a58fecefaaa95cf6500aa361c61cc9
Michael Okenwa Lawal <142639141+Michaellaw06@users.noreply.github.com>
2023-10-28 03:59:56 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
-
- * _pow - func calculates (base ^ power)
-
- * @base: base of the exponent
-
- * @power: power of the exponent
-
- *
-
- * Return: value of (base ^ power)
-
- */
+* _pow - func calculates (base ^ power)
+* @base: base of the exponent
+* @power: power of the exponent
+*
+* Return: value of (base ^ power)
+*/
 
 unsigned long int _pow(unsigned int base, unsigned int power)
 
